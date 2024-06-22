26a22121618f8024ee1250d26dbfc1da43ceb899
stefanusdeo <stefanusdeo07@gmail.com>
2024-06-22 22:39:48 +0700

    studycase Membership

--- studyCase/membership.sol
+++ studyCase/membership.sol
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.8.2 <0.9.0;
 
 contract MemberShip {
@@ -15,6 +14,39 @@ contract MemberShip {
 
     function removeMember(address _member) external {
         members[_member] = false;
+    }
+    
+}
+
+contract MemberShipUpgrade {
+    uint memberId;
+    struct Member {
+        uint256 Id;
+        string name;
+        uint256 balance;
+        bool membershipType;
+    }
+
+    enum memberType { Active, NotActive }
+    mapping (address => Member) members;
+
+    function addMember(address _member, string memory _name, uint256 _balance, memberType _membershipType) external {
+        memberId++; 
+
+        members[_member] = Member({
+            Id: memberId,
+            name: _name,
+            balance: _balance,
+            membershipType: _membershipType == memberType.Active
+        });
+    }
+    
+    function isMember(address _member) external view returns (bool) {
+        return members[_member].Id != 0;
+    }
+
+    function removeMember(address _member) external {
+        delete members[_member];
     }
     
 }
