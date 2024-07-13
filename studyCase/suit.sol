243021beccbff9c5b9c4e4652830dde1bf80662b
stefanusdeo <stefanusdeo07@gmail.com>
2024-07-13 10:55:30 +0700

    chore: forge init

---
+++ studyCase/suit.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.2 <0.9.0;
+
+contract RockPaperScissors {
+    enum Move { None, Rock, Paper, Scissors }
+
+    struct Player {
+        bytes32 commitment;
+        Move move;
+    }
+
+    mapping(address => Player) public players;
+    address public player1;
+    address public player2;
+    bool public gameEnded;
+
+    event GameResult(address winner, Move move1, Move move2);
+
+    constructor(address _player1, address _player2) {
+        player1 = _player1;
+        player2 = _player2;
+    }
+
+    function commitMove(Move _move, uint nonce) external {
+        require(msg.sender == player1 || msg.sender == player2, "Not a player");
+        require(players[msg.sender].commitment == 0, "Move already committed");
+
+        bytes32 commitment = keccak256(abi.encode(_move, nonce));
+        players[msg.sender].commitment = commitment;
+    }
+
+    function revealMove(Move _move, uint nonce) external {
+        require(players[msg.sender].commitment != 0, "Move not committed");
+        require(keccak256(abi.encode(_move, nonce)) == players[msg.sender].commitment, "Move does not match commitment");
+
+        players[msg.sender].move = _move;
+
+        if (players[player1].move != Move.None && players[player2].move != Move.None) {
+            determineWinner();
+        }
+    }
+
+    function determineWinner() private {
+        require(!gameEnded, "Game already ended");
+
+        Move move1 = players[player1].move;
+        Move move2 = players[player2].move;
+
+        address winner;
+
+        if (move1 == move2) {
+            winner = address(0); // Draw
+        } else if ((move1 == Move.Rock && move2 == Move.Scissors) ||
+                   (move1 == Move.Scissors && move2 == Move.Paper) ||
+                   (move1 == Move.Paper && move2 == Move.Rock)) {
+            winner = player1;
+        } else {
+            winner = player2;
+        }
+
+        gameEnded = true;
+        emit GameResult(winner, move1, move2);
+    }
+}
--- studyCase/vote.sol
+++ studyCase/vote.sol
@@ -59,3 +59,85 @@ contract Votting {
         return allCandidates;
     }
 }
+
+
+contract VotingTestQoin{
+    address public owner;
+    uint startDate;
+    uint endDate;
+    struct Candidate{
+        string name;
+        uint256 totalVote;
+    }
+    mapping (address => Candidate) public candidates;
+    mapping (address => bool) history;
+    address[] public candidateAddresses;
+    uint public candidateCount;
+
+    event historyVote(address vote, address selectedVote);
+
+    constructor() {
+        owner=msg.sender;
+    }
+
+    error OnlyOwner();
+
+    modifier onlyOwner(){
+        if(msg.sender != owner) revert OnlyOwner();
+        _;
+    }
+
+    function addCandidate(address candidateAddress, string calldata _name, uint256 _startDate, uint256 _endDate) external onlyOwner {
+        require(bytes(candidates[candidateAddress].name).length == 0, "number vote already exists");
+        require(_endDate > _startDate, "End date must be after start date");
+        candidateCount++;
+        candidates[candidateAddress] = Candidate({
+            name:_name,
+            totalVote:0
+        });
+
+        candidateAddresses.push(candidateAddress);
+        startDate=_startDate;
+        endDate=_endDate;
+    }
+
+    function voteCandidate(address addressCandidate, uint time)external {
+        require(bytes(candidates[addressCandidate].name).length > 0, "number vote not exists");
+        require(time < endDate && time > startDate, "not this time");
+        require(!history[msg.sender], "already vote");
+
+        history[msg.sender] = true ;
+        candidates[addressCandidate].totalVote++;
+
+        emit historyVote(msg.sender, addressCandidate);
+    }
+
+    function getResult(address addressCandidate)external view returns(string memory name, uint totalVote) {
+        Candidate memory candidate = candidates[addressCandidate];
+        require(bytes(candidate.name).length != 0, "Candidate does not exist");
+        return (candidate.name, candidate.totalVote);
+    }
+
+    function sendReward() external onlyOwner payable returns (string memory, address, uint) {
+        require(candidateCount > 0, "No candidates available");
+
+        address topCandidateAddress;
+        uint256 maxVotes = 0;
+
+        for (uint256 i = 0; i < candidateCount; i++) {
+            address candidateAddress = candidateAddresses[i];
+            if (candidates[candidateAddress].totalVote > maxVotes) {
+                maxVotes = candidates[candidateAddress].totalVote;
+                topCandidateAddress = candidateAddress;
+            }
+        }
+
+        Candidate memory topCandidate = candidates[topCandidateAddress];
+        (bool success, ) = topCandidateAddress.call{value: msg.value}("");
+        require(success, "Transfer failed");
+
+        return (topCandidate.name, topCandidateAddress, topCandidate.totalVote);
+    }
+
+
+}
